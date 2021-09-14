d931ee54f03530382e9d8352154cea988ef9caae
VladimirG <proton87@yandex.ru>
2021-09-14 11:39:57 +0300

    Initial Commit

---
+++ applicatura_weather_app/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  applicatura_weather_app
+//
+//  Created by Vladimir Gorbunov on 14.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
